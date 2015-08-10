3723f3024547211c33cf218eb2804c5b07fa9800
Yelena Belikova <yelenabel@gmail.com>
2015-08-10 13:50:53 -0700

    Prints out the ping pong test

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,11 +1,19 @@
 import java.util.Arrays;
 import java.util.ArrayList;
+import static java.lang.System.out;
 
 public class PingPong {
 	public static void main(String[] args) {
-
+		String userInput = "32";
+		ArrayList<String> arrayList;
+		if (validateInput(userInput) == true) {
+			arrayList = pingPong(Integer.parseInt(userInput));
+			for(Integer i = 0; i < Integer.parseInt(userInput); i++ ) {
+				System.out.println(arrayList.get(i));
+			}
+		}
 	}
-	public Boolean validateInput(String userInput) {
+	public static Boolean validateInput(String userInput) {
 		Integer input = 0;
 		try {
 			input = Integer.parseInt(userInput);
@@ -18,7 +26,7 @@ public class PingPong {
 		return true;
 	}
 
-	public ArrayList<String> pingPong(Integer userInput) {
+	public static ArrayList<String> pingPong(Integer userInput) {
 		ArrayList<String> pingPongList  = new ArrayList<String>();
 		String listOutput;
 
